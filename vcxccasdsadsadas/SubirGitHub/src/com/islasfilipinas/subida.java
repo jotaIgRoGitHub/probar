de47015574ebed8e4f0234dd1a36e8b9148e8e0d
jigle <jigle@LAPTOP-CVQTIDDV>
2023-03-21 10:28:20 +0100

    Inicio

---
+++ vcxccasdsadsadas/SubirGitHub/src/com/islasfilipinas/subida.java
@@ -0,0 +1,12 @@
+package com.islasfilipinas;
+
+public class subida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Esto es una subida a GitHub");
+		
+	}
+
+}
